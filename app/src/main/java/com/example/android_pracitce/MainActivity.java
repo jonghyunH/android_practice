659623fdb2367b004a29fa6e652e3259b9a5ecd0
jonghyunH <whdgus85845@gmail.com>
2020-11-07 21:27:43 +0900

    test2

--- app/src/main/java/com/example/android_pracitce/MainActivity.java
+++ app/src/main/java/com/example/android_pracitce/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android_pracitce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    Handler handler =new Handler();
+    Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent  = new Intent(getApplicationContext(), gameActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(run, 5000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(run);
     }
 }
---
+++ app/src/main/java/com/example/android_pracitce/gameActivity.java
@@ -0,0 +1,17 @@
+package com.example.android_pracitce;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class gameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+    }
+
+}
