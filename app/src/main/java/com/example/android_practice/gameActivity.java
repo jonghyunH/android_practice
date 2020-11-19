210fb38dac15202e5937fc42b0e9e3c056efa33f
jonghyunH <whdgus85845@gmail.com>
2020-11-20 01:22:30 +0900

    리사이클러뷰

--- app/src/main/java/com/example/android_practice/gameActivity.java
+++ app/src/main/java/com/example/android_practice/gameActivity.java
@@ -5,6 +5,10 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class gameActivity extends AppCompatActivity {
 
@@ -13,131 +17,18 @@ public class gameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
 
-        ImageButton btn1 = findViewById(R.id.imageButton1);
-        ImageButton btn2 = findViewById(R.id.imageButton2);
-        ImageButton btn3 = findViewById(R.id.imageButton3);
-        ImageButton btn4 = findViewById(R.id.imageButton4);
-        ImageButton btn5 = findViewById(R.id.imageButton5);
-        ImageButton btn6 = findViewById(R.id.imageButton6);
-        ImageButton btn7 = findViewById(R.id.imageButton7);
-        ImageButton btn8 = findViewById(R.id.imageButton8);
+        // 리사이클러뷰에 표시할 데이터 리스트 생성.
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            list.add(String.format("종현 %d", i));
+        }
 
-        final View box = findViewById(R.id.box);
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        //위 305
-        //아래 685
-        //왼 42
-        //오 880
-
-        btn1.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-                if(location[0] > 42 && location[1] > 305) {
-                    box.setX(location[0] - 30);
-                    box.setY(location[1] - 300);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
-        btn2.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-                if(location[1] > 305) {
-                    box.setY(location[1] - 300);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
-        btn3.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-                if(location[0] < 880 && location[1] > 305) {
-                    box.setX(location[0] + 30);
-                    box.setY(location[1] - 300);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
-        btn4.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-                if(location[0] > 42) {
-                    box.setX(location[0] - 30);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
-        btn5.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-                if(location[0] < 880) {
-                    box.setX(location[0] + 30);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
-        btn6.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-                if(location[0] > 42 && location[1] < 685) {
-                    box.setX(location[0] - 30);
-                    box.setY(location[1] - 200);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
-        btn7.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-                if(location[1] < 685) {
-                    box.setY(location[1] - 200);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
-        btn8.setOnClickListener(new ImageButton.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                int[] location = new int[2];
-                box.getLocationOnScreen(location);
-
-                if(location[0] < 880 && location[1] < 685){
-                    box.setX(location[0]+30);
-                    box.setY(location[1]-200);
-                }
-
-                System.out.println("x " + location[0] + " y " + location[1]);
-            }
-        });
-
+        // 리사이클러뷰에  객체 지정.
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
+        recyclerView.setAdapter(adapter);
     }
-
 }
